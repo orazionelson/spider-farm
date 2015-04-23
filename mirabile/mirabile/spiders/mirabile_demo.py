a2d923d6bc61f35892f56ec3e0b6a7c6c38e25d0
Vit Tucek <vit.tucek+wewe_git@gmail.com>
2015-04-23 13:46:44 +0200

    Fix logging from the demo spider

--- mirabile/mirabile/spiders/mirabile_demo.py
+++ mirabile/mirabile/spiders/mirabile_demo.py
@@ -16,10 +16,10 @@ class MirabileDemoSpider(scrapy.Spider):
         try:
             item[field] = sel.xpath(xpath).extract()[0]
         except:
-            scrapy.log.msg("Field %s not parsed correctly."%field)
+            self.log("Field %s not parsed correctly."%field)
 
     def parse(self, response):
-        scrapy.log.msg("Our starting URL is %s"%self.start_urls)        
+        self.log("Our starting URL is %s"%self.start_urls)
         item = MirabileItem()
         sel = response.selector.xpath('//td[@class="scheda_view"]')
         self.get_field('author', './/p/a/b/text()', sel, item)
@@ -31,15 +31,15 @@ class MirabileDemoSpider(scrapy.Spider):
         try:
             item['references'] = sel.xpath('.//p[4]/a/text()').extract()[0] + sel.xpath('.//p[4]/text()').extract()[-1]
         except:
-            scrapy.log.msg('Field references is not parsed correctly.')
+            self.log('Field references is not parsed correctly.')
         try:
             item['shelfmark'] = sel.xpath('./a/text()').extract()[0] + sel.xpath('./text()').extract()[-1] + ' ' + sel.xpath('./i/text()').extract()[-1]
         except:
-            scrapy.log.msg('Field shelfmark is not parsed correctly.')
+            self.log('Field shelfmark is not parsed correctly.')
         try:
             item['related_projects'] = sel.xpath('./a/text()').extract()[-1]
         except:
-            scrapy.log.msg('Field related_projects is not parsed correctly.')
+            self.log('Field related_projects is not parsed correctly.')
 
-        item['html'] = response.body_as_unicode()
+        #item['html'] = response.body_as_unicode()
         yield item
